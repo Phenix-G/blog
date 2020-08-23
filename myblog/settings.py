b2dfec6c2740ce94ae7fb337b97e43927c8fbca3
Chris-xcc <Chris_Guoc@163.com>
2020-08-23 21:02:25 +0800

    simple ui

--- myblog/settings.py
+++ myblog/settings.py
@@ -33,7 +33,7 @@ AUTH_USER_MODEL = 'user.User'
 # Application definition
 
 INSTALLED_APPS = [
-    # 'simpleui',
+    'simpleui',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
