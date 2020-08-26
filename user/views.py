bf34778da885cbb10897e4e34d07ffe04fb7151b
Chris-xcc <Chris_Guoc@163.com>
2020-08-26 16:28:24 +0800

    user info

--- user/views.py
+++ user/views.py
@@ -83,6 +83,21 @@ class UserViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.Upd
     def perform_create(self, serializer):
         return serializer.save()
 
+    # 用户个人资料修改
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response('密码修改成功', status=status.HTTP_200_OK)
+
     # 返回当前用户 /user/{id} id可以是任意值
     def get_object(self):
         return self.request.user
